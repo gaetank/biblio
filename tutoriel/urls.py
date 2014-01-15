431b6b2e261c9fd5cbee5d21dc024f4a753c0fca
Gaetan Kemayou <gaetan.kemayou@hotmail.fr>
2014-01-15 18:42:12 +0100

    ajout des routes pour la connexion et la deconnexion

--- tutoriel/urls.py
+++ tutoriel/urls.py
@@ -22,6 +22,4 @@ urlpatterns = patterns('',
      url(r'^ajouter_auteur/$', "biblio.views.ajouter_auteur"),
      url(r'^ajouter_livre/$', "biblio.views.ajouter_livre"),
      url(r'^login/?$', "biblio.views.login_page"),
-     url(r'^inscription/?$', "biblio.views.inscription"),
-     url(r'^logout/?$', "biblio.views.logout_action"),
 )
